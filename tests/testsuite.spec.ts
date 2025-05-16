b3b493df5a9026ef5b1dddc91c2ed532a526aeb9
Iftekhar Hossain <iftekhar.ctg30@gmail.com>
2025-05-16 18:06:17 +0200

    pipeline updates

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -9,13 +9,13 @@ import { KundvagnPage } from './pages/kundvagn-page';
 import { LeveransAdressPage} from './pages/leveransadress-page';
 
 
-test.beforeEach(async ({ page }) => {
+/*test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.goto();
   await loginPage.acceptCookiesIfVisible();
   await loginPage.openLoginDropdown();
   await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`);
-});
+});*/
 
 
 
@@ -25,8 +25,13 @@ test.describe('Sonepar Login Test Suite', () => {
 
 
   test('Test Case 1: Login and Startpage Overview', async ({ page }) => {
-   
-    //Verifiera så att det visas alla knapper efter inlogning
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.acceptCookiesIfVisible();
+    await loginPage.openLoginDropdown();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`);
+
+  //Verifiera så att det visas alla knapper efter inlogning
     const startPage = new StartPage(page);
     await startPage.verifyStartPageIsVisible();
     //Verifiera obejktnamn
